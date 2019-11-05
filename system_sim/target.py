de28e6eb45afce143b7f7c1893893faa01cb9ade
LiamAG <liam.adair.graham@gmail.com>
2019-11-05 09:52:19 -0600

    Wrote a naiive incidence function modelling scintillation. Added some docstrings.

--- system_sim/target.py
+++ system_sim/target.py
@@ -11,7 +11,29 @@ import numpy as np
 
 
 class target(object):
-	"""docstring for target"""
+	"""
+	Target object defining a simply scintillating target
+	Inputs:
+		str shape:  Specifies the shape of the target, supports
+				    prism and circle
+		tuple dims: Tuple specifying the dimensions of the target
+
+	Attributes:
+		float length: the length of the target in mm
+		float width:  the width of the target in mm
+		float depth:  the depth of the target in mm
+
+		float radius: radius of a circlular target
+
+		float light_output: light output by the target in photons/keV
+		float tau_decay_ns: slow decay time of the material in ns
+
+	Methods:
+		incidence: models the incidence of an electron beam on the target
+				   returns an np.ndarray object storing the number of
+				   photons produced by the beam distributed over the target
+				   dimensions
+	"""
 	def __init__(self, shape: str, dims: tuple):
 		"""initialization for target"""
 		if shape == 'prism':
@@ -24,8 +46,24 @@ class target(object):
 		else:
 			raise TypeError('{} is not an supported target geometry'.format{shape})
 
-		# Assume linear response to electrons and no decay time
+		# Assume linear response to electrons
 		# ie. 40 photons/keV deposited (NaI(Tl))
 		self.light_output = 40  # photons/keV
-		self.flourescence_time_ns = 0
 		self.tau_decay_ns = 70  # Ce:YAG
+
+	def incidence(self, beam):
+		"""
+		Models the process of scintillation when an electron beam is
+		incident on the target
+		Inputs:
+			beam: a beam object as defined in the beam module
+		Outputs:
+			photons: a numpy array containing the photons produced
+					 by the beam
+		"""
+		profile = beam.profile
+		energy = beam.energy
+		photons = np.zeros((beam.dims))
+		photons = profile * energy * self.light_output
+
+		return photons
