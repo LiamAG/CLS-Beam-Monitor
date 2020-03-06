3dc865ba576bbfeb416887fd03eb0deaf1c90a21
LiamAG <liam.adair.graham@gmail.com>
2020-03-05 19:38:39 -0600

    Image handler classes for diagnostic and background modes

--- basler-cls-linac/ImageHandlers.h
+++ basler-cls-linac/ImageHandlers.h
@@ -14,8 +14,9 @@
 using namespace Pylon;
 
 // Output directory variables
-char diagnosticImage[] = "./test_caps/grabbedImage.png";
-char backgroundImage[] = "./background.png";
+const char diagnosticImage[] = "test_caps/grabbedImage.png";
+const char backgroundImage[] = "background.png";
+
 
 // Image handler override classes
 class CImageEventBackground : public CImageEventHandler
@@ -23,6 +24,7 @@ class CImageEventBackground : public CImageEventHandler
 public:
 	virtual void OnImageGrabbed(CInstantCamera& camera, const CGrabResultPtr& ptrGrabResult);
 };
+
 
 class CImageEventDiagnostic : public CImageEventHandler
 {
