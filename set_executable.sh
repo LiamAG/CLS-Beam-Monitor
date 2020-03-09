939cbeb040a2abc0f305634336f46df1deaa6389
LiamAG <liam.adair.graham@gmail.com>
2020-03-09 10:06:46 -0600

    Removing uneeded or moved files

--- set_executable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# change the directory below into the targeted directory
-DIR="C:\Users\Marte\PycharmProjects\Capstone"
-inotifywait -m -r -e move -e create --format '%w%f' "$DIR" | while read f
-
-do
-  python ImageExample.py
-done
